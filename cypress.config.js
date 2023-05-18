54828525c8ceea99c20ba05222dde673d942e8f0
jmacielQA <jmacieltech@gmail.com>
2023-05-18 11:51:04 -0300

    Add cypress folder and cypress.config.js file

---
+++ cypress.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  projectId: "jn1iwh",
+  e2e: {
+    baseUrl: "http://localhost:8080",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};
